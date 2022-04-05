2890df8580b51de89cc285b76bbdb5ea2a9d5e6d
Vinci23 <88689746+Vinci23@users.noreply.github.com>
2022-04-05 13:09:07 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace klasaFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upiši svoje ime: ");
+            string ime = Console.ReadLine();
+
+            string datoteka = "ime.txt";
+
+            if (File.Exists(datoteka))
+            {
+                if (!Directory.Exists("backup"))
+                {
+                    Directory.CreateDirectory("backup");
+                }
+
+                File.Copy(datoteka, @"backup\ime_"
+                    + DateTime.Now.ToString("yyyy_MM_dd_HH_mm_ss")
+                    + ".txt");
+
+                File.Delete(datoteka);
+            }
+
+            File.WriteAllText(datoteka, ime);
+
+            Console.ReadKey();
+
+        }
+    }
+}
